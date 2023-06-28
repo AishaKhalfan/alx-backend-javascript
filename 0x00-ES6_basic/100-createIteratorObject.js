337fa697810699619f654ef7c112297097252f3c
AishaKhalfan <aishakhalfan45@gmail.com>
2023-06-28 22:49:32 +0300

    Iterator

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,12 @@
-//export default function createIteratorObject(report) {
-  //return (function* _() {
-    //for (const department of Object.values(report.allEmployees)) {
-      //for (const employee of department) {
-        //yield employee;
-      //}
-    //}
-  //}());
-//}
+// export default function createIteratorObject(report) {
+// return (function* _() {
+// for (const department of Object.values(report.allEmployees)) {
+// for (const employee of department) {
+// yield employee;
+// }
+// }
+// }());
+// }
 export default function createIteratorObject(report) {
   let result = [];
   for (const value of Object.values(report.allEmployees)) {
