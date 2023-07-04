1c74407b65665d7cede277ea23504ed09261f07e
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-05 00:32:04 +0300

    Retrieves ids from a list of students

--- 0x01-ES6_promise/promiseANY.js
+++ 0x01-ES6_promise/promiseANY.js
@@ -3,5 +3,14 @@ const promise2 = new Promise((resolve) => setTimeout(resolve, 100, 'quick'));
 const promise3 = new Promise((resolve) => setTimeout(resolve, 500, 'slow'));
 
 const promises = [promise1, promise2, promise3];
-
+//Promise.all()- returns the first fulfilled value
 Promise.any(promises).then((value) => console.log(value));
+
+const failures = new Promise((resolve, reject) => {
+  reject("Always fails");
+});
+
+Promise.any([failures]).catch((err) => {
+  console.log(err);
+});
+// AggregateError: No Promise in Promise.any was resolved
--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,2 +1,15 @@
-export default  function getListStudentIds(){
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
 }
