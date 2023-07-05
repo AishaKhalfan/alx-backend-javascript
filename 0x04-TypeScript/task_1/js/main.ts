fc4fb89341371fe31a8b33f49eb15fa8e4faeb48
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-05 21:40:14 +0300

    a function printTeacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,6 +11,15 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+function printTeacher(firstName, lastName){
+  const firstInitial = firstName[0];
+  const fullName = firstInitial + '.' + lastName;
+  return fullName;
+}
+
+interface printTeacherFunction{
+  (firstName: string, lastName:string) :string;
+}
 
 interface Studentint {
  firstName: string;
