73e3356c387b1c56ebcc86084866662174d6a0d4
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-05 13:56:41 +0300

    0x04-TypeScript

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,25 @@
+interface Student{
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+ }
+const student1: Student = {
+	firstName: 'Aisha',
+	lastName: 'Kay',
+	 age: 42,
+	 location: 'Kisumu',
+}; 
+
+const student2: Student = {
+	firstName: 'Atesh',
+	lastName: 'Achar',
+	 age: 50,
+	 location: 'Istanbul',
+}; 
+
+
+const studentsList = [student1, student2];
+
+
+ 
