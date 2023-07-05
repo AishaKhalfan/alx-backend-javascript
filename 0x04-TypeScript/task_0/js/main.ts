91d5697a7300d0c38da289ef1a81a49d8d334ebd
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-05 14:17:36 +0300

    Typescript

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -21,5 +21,35 @@ const student2: Student = {
 
 const studentsList = [student1, student2];
 
+const table = document.createElement("table");
+
+// Create table header row
+const headerRow = document.createElement("tr");
+const firstNameHeader = document.createElement("th");
+firstNameHeader.textContent = "First Name";
+headerRow.appendChild(firstNameHeader);
+const locationHeader = document.createElement("th");
+locationHeader.textContent = "Location";
+headerRow.appendChild(locationHeader);
+table.appendChild(headerRow);
+
+// Iterate through the students array and create rows
+studentsList.forEach((student) => {
+  const row = document.createElement("tr");
+
+  const firstNameCell = document.createElement("td");
+  firstNameCell.textContent = student.firstName;
+  row.appendChild(firstNameCell);
+
+  const locationCell = document.createElement("td");
+  locationCell.textContent = student.location;
+  row.appendChild(locationCell);
+
+  table.appendChild(row);
+});
+
+// Append the table to the document body or any desired element
+document.body.appendChild(table);
+
 
  
