207610a52e02d910fb30f7e0efb8dfd5489eec91
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-06 23:46:11 +0300

    Brand convention & Nominal typing

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,16 +1,14 @@
 export interface MajorCredits {
-    credits: Number;
-    brand: "Major";
+    credits: Number & {__brand: 'MajorCredits.credits' };
 
 }
 export interface MinorCredits {
-    credits: Number;
-    brand: "Minor";
+    credits: Number & {__brand: 'MinorCredits.credits' };
 }
 
 export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-    return { credits: subject1.credits + subject2.credits};
+    return { credits: subject1.credits + subject2.credits} as MajorCredits;
 }
 export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits{
-    return { credits: subject1.credits + subject2.credits};
+    return { credits: subject1.credits + subject2.credits} as MinorCredits;
 }
