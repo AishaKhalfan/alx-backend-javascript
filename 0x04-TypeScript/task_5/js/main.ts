c9a75807e520277e1ed8175a6d5cfe4663d4817f
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-06 23:56:35 +0300

    Nominal typing

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,12 +1,15 @@
 export interface MajorCredits {
-    credits: Number & {__brand: 'MajorCredits.credits' };
+    credits: number & {__brand: 'MajorCredits.credits' };
 
 }
 export interface MinorCredits {
-    credits: Number & {__brand: 'MinorCredits.credits' };
+    credits: number & {__brand: 'MinorCredits.credits' };
 }
 
-export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+export function sumMajorCredits(
+    subject1: MajorCredits,
+    subject2: MajorCredits
+    ): MajorCredits {
     return { credits: subject1.credits + subject2.credits} as MajorCredits;
 }
 export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits{
