67b39024c530e4a97a9ff4c674dd248108edb190
AishaKhalfan <aishakhalfan45@gmail.com>
2023-07-10 08:53:31 +0300

    Inheritance

---
+++ 0x02-ES6_classes/inheritance.js
@@ -0,0 +1,17 @@
+const parent = {
+  value: 2,
+  method() {
+    return this.value + 1;
+  },
+};
+
+console.log(parent.method()); // 3
+
+const child = {
+  __proto__: parent,
+};
+console.log(child.method()); // 3
+
+
+child.value = 4; 
+console.log(child.method()); // 5
---
+++ 0x02-ES6_classes/inheritance2.js
@@ -0,0 +1,60 @@
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(`${this.name} makes a noise.`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name) {
+    super(name); // call the super class constructor and pass in the name parameter
+  }
+
+  speak() {
+    console.log(`${this.name} barks.`);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name) {
+    super(name); // call the super class constructor and pass in the name parameter
+  }
+
+  speak() {
+    console.log(`${this.name} Meows.`);
+  }
+}
+
+const d = new Dog("Mitzie");
+d.speak(); // Mitzie barks.
+
+const c = new Cat("Atesh");
+c.speak(); // Atesh Meows
+
+//If there is a constructor present in the subclass, it needs to first call super() before using this. 
+//The super keyword can also be used to call corresponding methods of super class.
+class Simba {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(`${this.name} makes a noise.`);
+  }
+}
+
+class Lion extends Simba {
+  speak() {
+    super.speak();
+    console.log(`${this.name} roars.`);
+  }
+}
+
+const l = new Lion("Fuzzy");
+l.speak();
+// Fuzzy makes a noise.
+// Fuzzy roars.
+
