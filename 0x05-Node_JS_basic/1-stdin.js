458c8c6fcc2bf69ba814e797e3a7320232104dad
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-23 00:07:02 +0300

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,9 +6,15 @@ console.log('Welcome to Holberton School, what is your name?');
 
 readline.on('line', (name) => {
   console.log(`Your name is: ${name}`);
+
+  // Close interface for piped input
   readline.close();
 });
 
 readline.on('close', () => {
   console.log('This important software is now closing');
+
+  // Exit process completely on close
+  process.exit(0);
 });
+
