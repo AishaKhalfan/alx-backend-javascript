0a5127641a7ab75b81b89ad7600c785389e81f39
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-23 00:45:09 +0300

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,9 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const inputText = process.stdin.read();
+  if (inputText) process.stdout.write(`Your name is: ${inputText}`);
 });
-console.log('Welcome to Holberton School, what is your name?');
-
-readline.on('line', (name) => {
-  console.log(`Your name is: ${name}`);
-
-  // Close interface for piped input
-  readline.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
-
-readline.on('close', () => {
-  console.log('This important software is now closing');
-
-  // Exit process completely on close
-  process.exit(0);
-});
-
