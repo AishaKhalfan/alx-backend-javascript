1660d3453faba075eac53e4f1778d481962858b9
AishaKhalfan <aishakhalfan45@gmail.com>
2023-08-23 00:58:14 +0300

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,15 +1,38 @@
-const http = require('http');
+const fs = require('fs');
 
-const PORT = 1245;
+module.exports = async function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      const students = data
+        .split('\n')
+        .filter((content) => content)
+        .map((student) => student.split(','));
+      students.shift();
 
-const requestListener = (req, res) => {
-  res.writeHead(200);
-  res.end('Hello Holberton School!');
+      const messagesList = [];
+      const LENGTH = students.length ? students.length : 0;
+      messagesList.push(`Number of students: ${LENGTH}`);
+      const fields = {};
+
+      students.forEach((student) => {
+        if (student[3] in fields) fields[student[3]].push(student[0]);
+        else fields[student[3]] = [student[0]];
+      });
+
+      const fieldList = Object.keys(fields);
+      fieldList.forEach((field) => {
+        let message = `Number of students in ${field}: ${fields[field].length}.`;
+        message += ` List: ${fields[field].join(', ')}`;
+        messagesList.push(message);
+      });
+      const customMessage = messagesList.join('\n');
+      console.log(customMessage);
+      resolve(customMessage);
+    });
+  });
 };
 
-const app = http.createServer(requestListener);
-app.listen(PORT, () => {
-  console.log('...');
-});
-
-module.exports = app;
